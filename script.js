1acbde47ddc31aaa8dd323b60e90a8680366c8f8
Sidiq-coder <85211445+Sidiq-coder@users.noreply.github.com>
2024-03-17 14:16:11 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+const input = document.querySelector("textarea");
+const wordCount = document.querySelector("[data-word-count]")
+const characterCount = document.querySelector("[data-characters-count]")
+const sentenceCount = document.querySelector("[data-sentences-count]")
+const paragraphCount = document.querySelector("[data-paragraphs-count]")
+
+input.addEventListener("input", function(){
+    if(input.value){
+        //word
+        const wordsArray = input.value.split(" ").filter((word) => word != "");
+        wordCount.innerText = wordsArray.length;
+        //characters
+        characterCount.innerText = input.value.length;
+        //sentences
+        const sentencesArray = input.value.split(".");
+        sentenceCount.innerText = sentencesArray.length - 1;
+        //paragraph
+        const paragraphArray = input.value
+        .split("\n")
+        .filter((p) => p.trim() != "");
+        paragraphCount.innerText = paragraphArray.length;
+    } else{
+        wordCount.innerText = 
+        characterCount.innerText = 
+        sentenceCount.innerText = 
+        paragraphCount.innerText =
+        0;
+    }
+})
